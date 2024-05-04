f9d3488702221b48d06172bc369ea10a30e8e4e9
SAMANTHA2304 <90973235+SAMANTHA2304@users.noreply.github.com>
2024-05-04 10:29:16 -0600

    2024_2PARCIAL_SAMESCOBAR

---
+++ Actividad.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include "fstream"
+#include "cstdio"
+using namespace std;
+string numero,
+    }
+    if
+
+}
+
+void leernumero(string directorio)
+    cin >> ubicacionDestino;
+    rename("C:\Users\Usuario\Desktop\impar", ubicacionDestino);
+cin >> ubicacionDestino;
+rename("C:\Users\Usuario\Desktop\par", ubicacionDestino);
+
+//
+// Created by Samantha on 4/05/2024.
+//
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "archivos.h"
+
+
+using namespace  std;
+
+int main() {
+    int op;
+    cout << "¡Bienvenido!"<< endl;
+    cout << " ¿Cuantos archivos desea crear?" << endl << endl
+         << "Ingrese la cantidad:" << endl << endl;
+    cin >>
+
+if numero << 1+2
+        } break;
+
